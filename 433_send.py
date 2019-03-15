b4a721768fc5faf977f123e691c45bfcc37f7a10
Malte Dubrall <malte.dubrall@gmail.com>
2019-03-15 16:24:53 +0100

    add algorithm to send 433MHz data stream via GPIO 14

---
+++ 433_send.py
@@ -0,0 +1,63 @@
+import RPi.GPIO as GPIO
+import time
+
+variable = 0b000001011101
+GPIO_NUMBER = 14
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(GPIO_NUMBER, GPIO.OUT)
+
+
+def send_sync_bit():
+    GPIO.output(GPIO_NUMBER, GPIO.HIGH)
+    time.sleep(0.00015)
+    GPIO.output(GPIO_NUMBER, GPIO.LOW)
+    time.sleep(0.00045)
+
+
+def send_zero():
+    GPIO.output(GPIO_NUMBER, GPIO.HIGH)
+    time.sleep(0.00015)
+    GPIO.output(GPIO_NUMBER, GPIO.LOW)
+    time.sleep(0.00045)
+    GPIO.output(GPIO_NUMBER, GPIO.HIGH)
+    time.sleep(0.00015)
+    GPIO.output(GPIO_NUMBER, GPIO.LOW)
+    time.sleep(0.00045)
+
+
+def send_one():
+    GPIO.output(GPIO_NUMBER, GPIO.HIGH)
+    time.sleep(0.00015)
+    GPIO.output(GPIO_NUMBER, GPIO.LOW)
+    time.sleep(0.00045)
+    GPIO.output(GPIO_NUMBER, GPIO.HIGH)
+    time.sleep(0.00045)
+    GPIO.output(GPIO_NUMBER, GPIO.LOW)
+    time.sleep(0.00015)
+
+
+def send_data(input_data, bits_count):
+
+    for i in range(0, 5):
+
+        bits_count_work = bits_count
+        input_data_work = input_data
+
+        while bits_count_work > 0:
+
+            if input_data & (1 << 11):
+                send_one()
+            
+            else:
+                send_zero()
+            
+            bits_count_work -= 1
+            input_data_work = input_data_work << 1
+        
+        send_sync_bit()
+
+        #waiting 10ms between data stream
+        time.sleep(0.010)
