1a18bd81c7bb00d6e4bb01ea6b3d84f7bd9b1a5f
Malte Dubrall <malte.dubrall@gmail.com>
2019-03-20 18:06:07 +0100

    hallo kolja

--- lib/send.py
+++ lib/send.py
@@ -2,17 +2,25 @@ import RPi.GPIO as GPIO
 import time
 
 
-GPIO_NUMBER = 14
+SET = 4
+STATE = 14
+BIT_0 = 15
+BIT_1 = 17
+BIT_2 = 18
+
 
 short_delay = 0.000265
 long_delay = 0.000865
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(GPIO_NUMBER, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
+GPIO.setup(SET, GPIO.OUT)
+GPIO.setup(STATE, GPIO.OUT)
+GPIO.setup(BIT_0, GPIO.OUT)
+GPIO.setup(BIT_1, GPIO.OUT)
+GPIO.setup(BIT_2, GPIO.OUT)
 
-
+"""
 def send_sync_bit():
     GPIO.output(GPIO_NUMBER, GPIO.HIGH)
     time.sleep(short_delay)
@@ -66,11 +74,32 @@ def send_data(input_data):
 
         # Waiting 10ms between data packets.
         time.sleep(0.010)
+"""
 
-
+# send_command because just using arduino to transmit data via 433MHz
+# RPi -- parallel communication --> Arduino
 def send_command(device, action):
     
-    GPIO.output(14, device)
-    GPIO.output(15, action)
+    # -4 because BIT_2 (4, 2, 1)
+    if device - 4 >= 0:
+        device -= 4
+        GPIO.output(BIT_2, 1)
+    
+    if device - 2 >= 0:
+        GPIO.output(BIT_1, 1)
+        device -= 2
+    
+    if device - 1 >= 0:
+        GPIO.output(BIT_0, 1)
+        device -= 1
+
+    GPIO.output(STATE, action)
+    GPIO.output(SET, 1)
 
     time.sleep(0.5)
+
+    GPIO.output(SET, 0)
+    GPIO.output(STATE, 0)
+    GPIO.output(BIT_0, 0)
+    GPIO.output(BIT_1, 0)
+    GPIO.output(BIT_2, 0)
